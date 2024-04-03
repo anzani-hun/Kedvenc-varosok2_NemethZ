371474a8e175f1fe9e65e8420bcadc6a212eacbe
anzani-hun <anzani@hun>
2024-04-03 19:18:01 +0200

    Add project files.

---
+++ Kedvenc-varosok2_NemethZ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;   //ezt betölteni
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
