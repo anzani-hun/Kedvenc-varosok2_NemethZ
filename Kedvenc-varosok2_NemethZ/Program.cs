5535958d31d8b8236fd289f71b2baf55c2f1b62c
anzani-hun <anzani@hun>
2024-04-03 19:27:21 +0200

    óra vége

--- Kedvenc-varosok2_NemethZ/Program.cs
+++ Kedvenc-varosok2_NemethZ/Program.cs
@@ -9,6 +9,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        string connectionString, sqlStatement;
+        MySqlConnection dbConnection;
+        MySqlCommand sqlCommand;
 
+        connectionString = "server=localhost;userid=root;password=;database=mo_telepulesek";
+        dbConnection = new MySqlConnection(connectionString);
+        dbConnection.Open();
+
+
+        /*Hozzuk létre a lekérdezés parancsát kezelő `MySqlCommand` objektumot.
+         *A parancs a `kvarosok` tábla teljes tartalmát kérdezze le.*/
+        sqlStatement = "SELECT * FROM kvarosok;";
+        sqlCommand = new MySqlCommand(sqlStatement, dbConnection);
+
+
+        Console.ReadKey();
     }
 }
